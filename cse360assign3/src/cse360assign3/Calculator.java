cde5cf633b00ff2431d122d8ce87ef5a102b3900
Jorge Roldan <jroldan.267@gmail.com>
2016-02-20 15:33:09 -0700

    Number 3 for assignment 3

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -21,14 +21,14 @@ public class Calculator {
 	 * @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	/** Adds parameter value to the total
 	 * @param value
 	 * @return none
 	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	/** Subtracts parameter value to the total
@@ -36,7 +36,7 @@ public class Calculator {
 	 * @return none
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	/** Multiplies parameter value to the total
@@ -44,7 +44,7 @@ public class Calculator {
 	 * @return none
 	 */
 	public void multiply (int value) {
-		
+		total = total*value;
 	}
 	
 	/** Divides the total by the parameter value. If value is 0, set total to 0.
@@ -52,7 +52,10 @@ public class Calculator {
 	 * @return none
 	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	
 	/** Returns empty string
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -14,32 +14,54 @@ public class CalculatorTest {
 	
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		assertEquals(0,calc.getTotal());
 	}
 	
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(4);
+		assertEquals(4, calc.getTotal());
 	}
 	
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(4);
+		assertEquals(4, calc.getTotal());
+		calc.subtract(2);
+		assertEquals(2, calc.getTotal());
 	}
 	
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(4);
+		assertEquals(4, calc.getTotal());
+		calc.multiply(2);
+		assertEquals(8, calc.getTotal());
 	}
 	
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(4);
+		assertEquals(4, calc.getTotal());
+		
+		calc.divide(0);
+		assertEquals(0, calc.getTotal());
+		
+		calc.add(4);
+		assertEquals(4, calc.getTotal());
+		calc.divide(2);
+		assertEquals(2, calc.getTotal());
 	}
 	
 	@Test
 	public void testGetHistory() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		assertEquals("", calc.getHistory());
 	}
 }
 
