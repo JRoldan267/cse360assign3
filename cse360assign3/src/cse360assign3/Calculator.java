61dd7abf16dd7a10f39d9008c16a87401958cf86
Jorge Roldan <jroldan.267@gmail.com>
2016-02-20 15:10:59 -0700

    Number two for the assignment

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,33 +1,64 @@
 package cse360assign3;
+/** Calculator class for assignment 3.
+ * @author Jorge Roldan
+ *
+ */
+
 
 public class Calculator {
 
-	private int total;
+	private int total; // private int that keeps track of total
 	
+	/** Constructor class for Calculator
+	 * @param none
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/** Returns total
+	 * @param none
+	 * @return total
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+	/** Adds parameter value to the total
+	 * @param value
+	 * @return none
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/** Subtracts parameter value to the total
+	 * @param value
+	 * @return none
+	 */
 	public void subtract (int value) {
 		
 	}
 	
+	/** Multiplies parameter value to the total
+	 * @param value
+	 * @return none
+	 */
 	public void multiply (int value) {
 		
 	}
 	
+	/** Divides the total by the parameter value. If value is 0, set total to 0.
+	 * @param value
+	 * @return none
+	 */
 	public void divide (int value) {
 		
 	}
 	
+	/** Returns empty string
+	 * @param none
+	 * @return ""
+	 */
 	public String getHistory () {
 		return "";
 	}
